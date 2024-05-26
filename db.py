5d1bea8258399f863e5c8967d98625a6e360f97a
Roman Matyushkin <144627459+amroman1337@users.noreply.github.com>
2024-05-26 22:27:33 +0300

    added places for priorityes

--- db.py
+++ db.py
@@ -2,9 +2,17 @@ import sqlite3
 
 db_lp = sqlite3.connect('storage.db')
 cursor_db = db_lp.cursor()
+
 sql_create = '''CREATE TABLE IF NOT EXISTS passwords(
                 login TEXT PRIMARY KEY,
-                password TEXT NOT NULL);'''
+                password TEXT NOT NULL,
+                movietype1 TEXT,
+                movietype2 TEXT,
+                movietype3 TEXT,
+                booktype1 TEXT,
+                booktype2 TEXT,
+                booktype3 TEXT);'''
+
 cursor_db.execute(sql_create)
 db_lp.commit()
 
